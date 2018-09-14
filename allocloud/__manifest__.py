5e4011ce5a4043602a1bcd9c50c064b80015b280
Ludovic Gasc (GMLudo) <gmludo@gmail.com>
2018-09-14 14:10:36 +0200

    ALLOcloud fake app for Odoo store, second try

---
+++ allocloud/__manifest__.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "ALLOcloud",
+
+    'summary': """ALLOcloud summary""",
+
+    'description': """
+        ALLOcloud summary
+    """,
+
+    'author': "ALLOcloud",
+    'website': "https://www.allocloud.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/master/odoo/addons/base/module/module_data.xml
+    # for the full list
+    'category': 'Extra Tools',
+    'version': '1.0',
+
+    # any module necessary for this one to work correctly
+    'depends': ['web'],
+
+    # always loaded
+    'data': [
+        # 'security/ir.model.access.csv',
+        #'templates.xml',
+    ],
+    # only loaded in demonstration mode
+    # 'demo': [
+    #     'demo.xml',
+    # ],
+}
