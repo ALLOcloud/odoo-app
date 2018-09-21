060ca9d5d05495791b897cd0e4f5ae80e921fd89
William Nemencha <william.nemencha@allocloud.com>
2018-09-21 14:14:26 +0200

    docs(manifest): Add marketing texts.

--- allocloud/__manifest__.py
+++ allocloud/__manifest__.py
@@ -2,10 +2,13 @@
 {
     'name': "ALLOcloud",
 
-    'summary': """ALLOcloud summary""",
+    'summary': "Integration of Odoo with the ALLOcloud VoIP telephony platform for advanced CTI functionalities",
 
     'description': """
-        ALLOcloud description
+        The ALLOcloud-Odoo integration allows to leverage your telephony directly from Odoo.
+        
+        Click-to-call, popup notification on incoming call are the basic CTI (Computer Telephony Integration) built-in features. 
+        You can go further by using the Odoo and ALLOcloud APIs and build your own integrations (add calls to the contact activity log, attach call recordings to a specific contact, time call duration for invoicing etc.)
     """,
 
     'author': "ALLOcloud",
